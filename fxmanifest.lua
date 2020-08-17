5d96c4eb73e39f3cc212b67e2975df4ee2b79cf6
Pablo Zapata <42814853+PABLO-1610@users.noreply.github.com>
2020-08-17 18:00:35 +0300

    Initialize project manifest.

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'adamant'
+game 'gta5'
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+version '1.0.0'
+
+client_scripts {
+    'config/main.lua',
+    
+    'client/camera.lua',
+    'client/functions.lua',
+    'client/quests.lua',
+
+}
+
+server_scripts {
+    'config/main.lua',
+}
+
+
+
