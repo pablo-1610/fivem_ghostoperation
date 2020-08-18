c02a17920661cbaa1414d33012f4b6cec494fcc5
Pablo Zapata <42814853+PABLO-1610@users.noreply.github.com>
2020-08-18 13:59:29 +0300

    Updating data loading animation

--- client/cache.lua
+++ client/cache.lua
@@ -2,27 +2,27 @@ local playerData = {}
 local playerDataSync = false
 local localServerData = {}
 
-AddEventHandler("onClientMapStart", function()
+AddEventHandler('onClientMapStart', function()
+    Citizen.Wait(2500)
+    DoScreenFadeIn(800)
     -- Démarrage des systèmes de caches
-	exports.spawnmanager:spawnPlayer()
-	Citizen.Wait(5000)
     exports.spawnmanager:setAutoSpawn(false)
 
-    TriggerServerEvent(sysPrefix.."getPlayerData")
+    
+    Citizen.Wait(1500)
+   
 
-    DoScreenFadeOut(800)
+    DoScreenFadeOut(100)
 
     while not IsScreenFadedOut() do Citizen.Wait(50) end
-    
-
-
     Ghost.showLoading("Chargement de vos données")
+    Citizen.Wait(9000)
+    TriggerServerEvent(sysPrefix.."getPlayerData")
 
     while not playerDataSync do Citizen.Wait(100) end
-
     Ghost.showLoading(false)
     Ghost.initializeGame()
-    DoScreenFadeIn(800)
+    DoScreenFadeIn(100)
 end)
 
 RegisterNetEvent(sysPrefix.."receivePlayerData")
